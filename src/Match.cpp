ba39fb211315f0b7f6051813a27e1693a79188c4
EmperorNao <59420696+EmperorNao@users.noreply.github.com>
2022-01-07 11:49:57 +0300

    change match

--- src/Match.cpp
+++ src/Match.cpp
@@ -12,6 +12,11 @@ void Match::simulate(int seed) {
 
 Schedule generate_schedule(std::vector<Team> teams) {
 
+	int times_to_play = 2;
+	int matches_for_each_team = (teams.size() - 1) * times_to_play;
 
+	int days = matches_for_each_team * 2;
+	
+	return Schedule{};
 
 }
