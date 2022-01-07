dc06817f14b3296e2732b67d7e4618f6cdea25c0
lexus1demotivator <yamakasi1227@gmail.com>
2022-01-07 11:52:35 +0300

    GameManeSceneContent

--- src/scenes/MainGameScene.cpp
+++ src/scenes/MainGameScene.cpp
@@ -3,12 +3,18 @@
 
 void MainGameScene::content() {
 
-
+	std::cout << "Игровая лига" << std::endl;
+	std::cout << "Выберите действие" << std::endl;
+	std::cout << "1 - Просмотреть расписание матчей" << std::endl;
+	std::cout << "2 - Просмотреть текущую статистику" << std::endl;
+	std::cout << "3 - Управление командой" << std::endl;
+	std::cout << "s - Начать следующий матч" << std::endl;
 
 }
 
 Scene* MainGameScene::options(std::string message) {
 
+	
 
 
 }
